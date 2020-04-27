d3360998bb39834f91d73f0c55ff75b0361b0fde
樊千千 <fanqianweb@gmail.com>
2020-04-27 23:45:49 +0800

    七牛webpack插件

---
+++ webpack-plugin/plugins/qiniuUpload.js
@@ -0,0 +1,76 @@
+const qiniu = require('qiniu');
+const path = require('path');
+
+class qiniuUploadPlugin {
+  // 七牛SDK mac对象
+  mac = null;
+
+  constructor(options) {
+    // 读取传入选项
+    this.options = options || {};
+    // 检查选项中的参数
+    this.checkQiniuConfig();
+    // 初始化七牛mac对象
+    this.mac = new qiniu.auth.digest.Mac(
+      this.options.qiniu.accessKey,
+      this.options.qiniu.secretKey
+    );
+  }
+  checkQiniuConfig() {
+    // 配置未传qiniu，读取环境变量中的配置
+    if (!this.options.qiniu) {
+      this.options.qiniu = {
+        accessKey: process.env.QINIU_ACCESS_KEY,
+        secretKey: process.env.QINIU_SECRET_KEY,
+        bucket: process.env.QINIU_BUCKET,
+        keyPrefix: process.env.QINIU_KEY_PREFIX || ''
+      };
+    }
+    const qiniu = this.options.qiniu;
+    if (!qiniu.accessKey || !qiniu.secretKey || !qiniu.bucket) {
+      throw new Error('invalid qiniu config');
+    }
+  }
+
+  apply(compiler) {
+      compiler.hooks.afterEmit.tapPromise('MyWebpackPlugin', (compilation) => {
+            returnnewPromise((resolve, reject) => {
+                  // 总上传数量
+                  const uploadCount = Object.keys(compilation.assets).length;
+                  // 已上传数量
+                  let currentUploadedCount = 0;
+                  // 七牛SDK相关参数
+                  const putPolicy = new qiniu.rs.PutPolicy({ scope: this.options.qiniu.bucket });
+                  const uploadToken = putPolicy.uploadToken(this.mac);
+                  const config = new qiniu.conf.Config();
+                  config.zone = qiniu.zone.Zone_z1;
+                  const formUploader = new qiniu.form_up.FormUploader()
+                  const putExtra = new qiniu.form_up.PutExtra();
+                  // 因为是批量上传，需要在最后将错误对象回调
+                  let globalError = null;
+
+                  // 遍历编译资源文件
+                  for (const filename of Object.keys(compilation.assets)) {
+                    // 开始上传
+                    formUploader.putFile(
+                        uploadToken,
+                        this.options.qiniu.keyPrefix + filename,
+                        path.resolve(compilation.outputOptions.path, filename),
+                        putExtra,
+                        (err) => {
+                          console.log(`uploade ${filename} result: ${err ? `Error:${err.message}` : 'Success'}`)
+                            currentUploadedCount++;
+                            if (err) {
+                                globalError = err;
+                            }
+                            if (currentUploadedCount === uploadCount) {
+                                globalError ? reject(globalError) : resolve();
+                            }
+                        });
+                }
+            })
+        });
+    }
+}
+
+module.exports = qiniuUploadPlugin;
--- webpack-plugin/src/components/App.js
+++ webpack-plugin/src/components/App.js
@@ -4,14 +4,14 @@ import Icon from '../assets/logo.png';
 import '../styles/index.less'
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <div>
-        <img src={Icon}/>
+        <img src={Icon} />
         <p>react demo</p>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App/> , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
