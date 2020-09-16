d00184c87ed83e919c0afde18bae280d6a2ac46b
樊千千 <fanqianweb@gmail.com>
2020-09-16 21:40:52 +0800

    1

--- test.js
+++ test.js
@@ -1,44 +1,17 @@
-const tree = {
-  name: 'root',
-  children: [
-    { name: '叶子1-1' },
-    { name: '叶子1-2' },
-    {
-      name: '叶子2-1',
-      children: [{
-        name: '叶子3-1',
-        children: [{
-          name: '叶子4-1'
-        }]
-      }]
+var isValid = function(s) {
+  var rightSymbols = []
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] == '(') {
+      rightSymbols.push(')')
+    } else if (s[i] == '{') {
+      rightSymbols.push('}')
+    } else if (s[i] == '[') {
+      rightSymbols.push(']')
+    } else if (rightSymbols.pop() != s[i]) {
+      return false
     }
-  ]
-}
-
-
-function getDepth(tree) {
-  let depth = 0
-
-  if (tree) {
-    let arr = [tree]
-    let temp = arr
-    while (temp.length) {
-      arr = temp
-      temp = []
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i].children && arr[i].children.length) {
-          for (let j = 0; j < arr[i].children.length; j++) {
-            temp.push(arr[i].children[j])
-          }
-        }
-      }
-
-      if (temp.length >= 0) {
-        depth++
-      }
-    }
+    console.log(rightSymbols)
   }
-
-  return depth
+  return !rightSymbols.length
 }
-console.log(getDepth(tree))
+console.log(isValid("[]"))
